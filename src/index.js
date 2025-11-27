340dde095ea6281824062f38818a5f6e5b515dc5
nordwaldberg <nord.waldberg@gmail.com>
2025-11-27 23:07:42 +0300

    Add target path builder function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+// import axios from 'axios'
+// import fs from 'fs/promises'
+// import path from 'path'
+
 const pageLoader = (url, output) => {
-  return [url, output]
+  return `open ${getAbsolutePathToTarget(url, output)}`
+}
+
+const getAbsolutePathToTarget = (filepath, targetDir) => {
+  const urlData = filepath.replace(/https?:\/\//i, '').split('/')
+  const targetUrl = urlData[0].replace(/[^a-zA-Z0-9]/g, '-')
+  const targetFile = urlData[urlData.length - 1] + '.html'
+
+  return targetDir + '/' + [targetUrl, targetFile].join('-')
 }
 
 export default pageLoader
