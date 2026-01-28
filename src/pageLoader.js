acff46e0c9e3497490dd2d651986ce51ae04ab2b
nordwaldberg <nord.waldberg@gmail.com>
2026-01-28 17:38:22 +0300

    Fix review comments, refactor pageLoader html and resources processing

--- src/pageLoader.js
+++ src/pageLoader.js
@@ -22,12 +22,55 @@ const getAbsolutePathToTarget = (url, outputDir) => {
   return path.join(outputDir, filename)
 }
 
+const extractResources = ($, baseUrl, pageUrl) => {
+  log('Extracting resources...')
+
+  const resources = []
+
+  resourceTypes.forEach(({ selector, attr }) => {
+    $(selector).each((i, el) => {
+      const src = $(el).attr(attr)
+
+      if (!src) return
+
+      const resourceUrl = src.startsWith('http') ? src : new URL(src, baseUrl).href
+      const resourceObj = new URL(resourceUrl)
+
+      if (resourceObj.hostname !== pageUrl.hostname) return
+
+      resources.push({ el, attr, resourceUrl })
+    })
+  })
+
+  return resources
+}
+
+const createResourceTasks = ($, resources, resourcesDir) => {
+  log('Creating tasks...')
+
+  return resources.map(({ el, attr, resourceUrl }) => ({
+    title: resourceUrl,
+    task: () => downloadResource(el, attr, resourceUrl, resourcesDir, $),
+  }))
+}
+
+const processHtml = (html, url, resourcesDir, pageUrl) => {
+  const $ = cheerio.load(html)
+
+  const resources = extractResources($, url, pageUrl)
+  const tasks = createResourceTasks($, resources, resourcesDir)
+
+  return { $, tasks }
+}
+
 const pageLoader = (url, output = process.cwd()) => {
   log('Downloading webpage...')
 
   const targetPath = getAbsolutePathToTarget(url, output)
   const resourcesDir = targetPath.replace(/\.html$/, '_files')
   const pageUrl = new URL(url)
+
+  let $
 
   return axios.get(url)
     .then(response => response.data)
@@ -40,43 +83,22 @@ const pageLoader = (url, output = process.cwd()) => {
     .then((html) => {
       log('Creating resources directory...')
 
-      const $ = cheerio.load(html)
-
-      return fs.mkdir(resourcesDir).then(() => $)
+      return fs.mkdir(resourcesDir).then(() => html)
     })
-    .then(($) => {
-      log('Downloading webpage resources...')
+    .then((html) => {
+      log('Processing...')
 
-      const resourcesTasks = []
-
-      resourceTypes.forEach(({ selector, attr }) => {
-        $(selector).each((i, el) => {
-          const src = $(el).attr(attr)
-
-          if (!src) {
-            return
-          }
-
-          const resourceUrl = src.startsWith('http') ? src : new URL(src, url).href
-          const resourceObj = new URL(resourceUrl)
-
-          if (resourceObj.hostname !== pageUrl.hostname) return
-
-          resourcesTasks.push({
-            title: resourceUrl,
-            task: () => downloadResource(el, attr, resourceUrl, resourcesDir, $),
-          })
-        })
-      })
+      const result = processHtml(html, url, resourcesDir, pageUrl)
+      $ = result.$
 
       return new Listr(
-        resourcesTasks,
+        result.tasks,
         {
           concurrent: true,
           renderer: process.env.NODE_ENV === 'test' ? 'silent' : 'default',
-        }).run().then(() => $)
+        }).run()
     })
-    .then(($) => {
+    .then(() => {
       log('Creating webpage html file...')
 
       return fs.writeFile(targetPath, $.html()).catch((err) => {
